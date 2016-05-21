4c7af59c1a34128310e874bbedb7c775b29b008f
hrib <hugo.dose@gmail.com>
2016-05-21 15:20:31 +0100

    Update follow.php

--- example/follow.php
+++ example/follow.php
@@ -9,7 +9,7 @@ $instagram = new Instagram(array(
   'apiCallback' => 'YOUR_APP_CALLBACK' // must point to success.php
 ));
 // set user's accesstoken (can be received after authentication)
-$instagram->setAccessToken("2823787.9687902.21u77429n3r79o08233122306fa78902");
+$instagram->setAccessToken("2105655638.cf2cd88.244f31b9283d43488b0852b6479d7398");
 // follow user (snoopdogg)
 $result = $instagram->modifyRelationship('follow', 1574083);
 // receive the list of users this user follows
