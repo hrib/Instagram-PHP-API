84080eba1b0903c502cc81b54144d2c6ddb612a4
hrib <hugo.dose@gmail.com>
2016-05-21 17:18:55 +0100

    Update follow.php

--- example/follow.php
+++ example/follow.php
@@ -13,8 +13,8 @@ $instagram->setAccessToken("2105655638.cf2cd88.244f31b9283d43488b0852b6479d7398"
 // follow user (snoopdogg)
 $result = $instagram->modifyRelationship('follow', 1574083);
 // receive the list of users this user follows
-$follows = $instagram->getUserFollows();
+//$follows = $instagram->getUserFollows();
 // dump response object
 echo '<pre>';
-print_r($follows);
+//print_r($follows);
 echo '<pre>';
