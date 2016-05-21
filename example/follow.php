7232e8db9689e671b9141c0ed140f5337ae8d4e8
hrib <hugo.dose@gmail.com>
2016-05-21 17:13:26 +0100

    Update follow.php

--- example/follow.php
+++ example/follow.php
@@ -4,9 +4,9 @@ require '../src/Instagram.php';
 //use MetzWeb\Instagram\Instagram;
 
 $instagram = new Instagram(array(
-  'apiKey'      => 'YOUR_APP_KEY',
+  'apiKey'      => 'Dcf2cd88',
   'apiSecret'   => 'YOUR_APP_SECRET',
-  'apiCallback' => 'YOUR_APP_CALLBACK' // must point to success.php
+  'apiCallback' => 'http://instafollow.elasticbeanstalk.com/' // must point to success.php
 ));
 // set user's accesstoken (can be received after authentication)
 $instagram->setAccessToken("2105655638.cf2cd88.244f31b9283d43488b0852b6479d7398");
