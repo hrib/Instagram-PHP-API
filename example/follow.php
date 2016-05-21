a485f0f816a4ff4f3e58de996d372d13be49067a
hrib <hugo.dose@gmail.com>
2016-05-21 17:54:10 +0100

    Update follow.php

--- example/follow.php
+++ example/follow.php
@@ -9,7 +9,7 @@ $instagram = new Instagram(array(
   'apiCallback' => 'http://instafollow.elasticbeanstalk.com/' // must point to success.php
 ));
 // set user's accesstoken (can be received after authentication)
-$instagram->setAccessToken("2105655638.cf2cd88.244f31b9283d43488b0852b6479d7398");
+$instagram->setAccessToken("xxx2105655638.cf2cd88.244f31b9283d43488b0852b6479d7398");
 // follow user (snoopdogg)
 $result = $instagram->modifyRelationship('follow', 3235184663);
 // receive the list of users this user follows
